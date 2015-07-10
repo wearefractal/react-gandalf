435194543090f6cbef80395c23764177e1a11662
Contra <contra@maricopa.edu>
2015-07-09 18:08:22 -0700

    initial version

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 'use strict';
 
-module.exports = {};
+var React = require('react');
+var merge = require('lodash.merge');
+
+var DOM = React.DOM;
+var PropTypes = React.PropTypes;
+
+module.exports = React.createClass({
+  displayName: 'Gandalf',
+  propTypes: {
+    allowBack: PropTypes.bool,
+    steps: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        skippable: PropTypes.bool,
+        component: PropTypes.func
+      })
+    )
+  },
+  previousStep: function(){
+    // TODO
+  },
+  nextStep: function(){
+    this.refs.stepper.next();
+  },
+  getSteps: function(){
+    return this.props.steps.map(transformStep, this);
+  },
+  render: function(){
+    var props = merge({
+      ref: 'stepper',
+      steps: this.getSteps()
+    }, this.props);
+
+    return MultiStep(props);
+  }
+});
+
+function transformStep(cfg){
+  var goBack = this.props.allowBack ? this.previousStep : null;
+  return {
+    name: cfg.name,
+    component: cfg.component({
+      skippable: cfg.skippable,
+      nextStep: this.nextStep,
+      previousStep: goBack
+    })
+  };
+}
